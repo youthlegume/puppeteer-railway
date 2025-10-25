bea396f701e494d0fd7f163972f2f8ffbee59fe6
youthlegume <benparker801@gmail.com>
2025-10-25 12:15:19 -0700

    Add ultra-simple test server

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 8080;
+
+app.get('/', (req, res) => {
+  res.json({ message: 'Hello from Railway!' });
+});
+
+app.get('/health', (req, res) => {
+  res.json({ status: 'OK' });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
